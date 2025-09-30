a6822782ff3bf908b51c8207bc7b90c82d4a9f7c
rodrigues.cco <bielcreations123@gmail.com>
2025-09-30 15:30:04 -0300

    just testing

--- insert_request_test.py
+++ insert_request_test.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "http://127.0.0.1:8000/insert"
-files = {"file": open(r"faces\alexandre.jpg", "rb")}
+files = {"file": open(r"faces\Silveira.jpg", "rb")}
 params = {"name": "Silveira"}  # query parameter
 
 resp = requests.post(url, files=files, params=params)
--- recognize_request_test.py
+++ recognize_request_test.py
@@ -1,6 +1,6 @@
 import requests
 
 url = "http://127.0.0.1:8000/recognize"
-files = {"file": open(r"uploaded_images\2025-09-30 12-50-53.088346.jpg", "rb")}
+files = {"file": open(r"faces\alexandre.jpg", "rb")}
 resp = requests.post(url, files=files)
 print(resp.json())
